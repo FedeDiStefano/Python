c9725caee8c58f23015e2e9dcd97c483074e4547
FedeDiStefano <fededistefano08@gmail.com>
2022-01-25 01:55:33 -0300

    123

--- Curso de Python/tabla de multiplicacion con while.py
+++ Curso de Python/tabla de multiplicacion con while.py
@@ -1,16 +1,7 @@
 tabla = int(input('introduzca una tabla de multiplicacion: '))
-count = 1 
-max_number = 10 
+count = 1
+max_number = 10
 
-while count <= max_number: 
+while count <= max_number:
     print(f'{tabla} x {count} = {tabla * count}')
     count = count + 1
-
-
-
-
-
-
-
-
-
