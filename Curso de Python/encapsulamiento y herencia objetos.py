16e879ff321ff1b31c4f89a3fa1d0beda542e3f6
FedeDiStefano <fededistefano08@gmail.com>
2022-01-27 14:28:27 -0300

    27/01/2022

--- Curso de Python/encapsulamiento y herencia objetos.py
+++ Curso de Python/encapsulamiento y herencia objetos.py
@@ -17,18 +17,19 @@ class celular:
     def get_prendido(self):
         return self.__prendido
 
+class CelularApple:
+    MPX = 12.2
 
-
-class CelularBueno(celular):
+class CelularBueno(celular, CelularApple):
     GB = 60
 
 
-    def __init__(self, marca, color, modelo, GB):
+    def __init__(self, marca, color, modelo, GB, MPX):
         self. marca = marca
         self. color = color
         self. modelo = modelo
         self. GB =  GB 
-
+        self. MPX = MPX
 
 celular1 = celular("samsung","blanco", "galaxy J9")
 #celular1.prender()
@@ -38,8 +39,8 @@ print(f'marca: {celular1.marca}, color: {celular1.color}, modelo: {celular1.mode
 print(f'prendido: {celular1.get_prendido()}')
 
 
-celular_alta_gama = CelularBueno("samsung", "negro", "galaxy s21 ultra", 512)
-print(f'marca: {celular_alta_gama.marca}, color: {celular_alta_gama.color}, modelo: {celular_alta_gama.modelo}, GB: {celular_alta_gama.GB}')
+celular_alta_gama = CelularBueno("samsung", "negro", "galaxy s21 ultra", 512, 108)
+print(f'marca: {celular_alta_gama.marca}, color: {celular_alta_gama.color}, modelo: {celular_alta_gama.modelo}, GB: {celular_alta_gama.GB}, MPX camara trasera: {celular_alta_gama.MPX} ')
 
 
 
