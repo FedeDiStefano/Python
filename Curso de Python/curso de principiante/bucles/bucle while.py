6e0b41dfcfc557740d040b6a3cc6423794463734
FedeDiStefano <fededistefano08@gmail.com>
2022-02-02 16:44:44 -0300

    02/02/2022

--- Curso de Python/curso de principiante/bucles/bucle while.py
+++ Curso de Python/curso de principiante/bucles/bucle while.py
@@ -1,20 +1,11 @@
-count = 1
-while count <= 50: 
-    print(f'hola --> {count}')
-    count = count  +1
+#count = 1
+# while count <= 50:
+#print(f'hola --> {count}')
+#count = count  +1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+a = 0
+while True:
+    print(a)
+    if a >= 3:
+        break
+    a += 1
--- Curso de Python/segunda clase/prueba.py
+++
@@ -1,6 +0,0 @@
-a = ("hola")
-b = 10 * 10
-print(type(a), type(b))
-
-c = (b - 23)
-print(c)
