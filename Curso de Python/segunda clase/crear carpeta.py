f3f1934bf1ae25bbacbbda7d5ac2e0e5a1e8eb88
FedeDiStefano <fededistefano08@gmail.com>
2022-02-03 18:30:53 -0300

    03/02/2022

--- Curso de Python/segunda clase/crear carpeta.py
+++ Curso de Python/segunda clase/crear carpeta.py
@@ -3,7 +3,10 @@ import os
 nueva_carpeta = input("el nombre de la nueva carpeta: ")
 directorio_actual = os.getcwd()
 print("este es su directorio:", directorio_actual)
-# PS E:\Python
 path = os.path.join(directorio_actual, nueva_carpeta)
-#print("nueva carpeta ruta:", path)
-os.mkdir(path)
+
+if os.path.exists(path):
+    print("la carpeta ya existe")
+else:
+    os.mkdir(path)
+    print("carpeta creada")
